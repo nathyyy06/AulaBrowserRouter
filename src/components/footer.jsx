1139e8c78bcdb0de980cc81b298ac80b2274dd21
nathyyy06 <nathalia.cruz@estudante.ifms.edu.br>
2024-08-23 15:26:13 -0300

    entrega

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,10 +4,10 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p class="p">Telefone: 0800 0 11 5566</p>
-        <p class="p"> E-mail: Natura@gmail.com</p>
-        <p class="p">Instagram: @naturaOficial </p>
-        <p class="p">© 2024 Natura.</p>
+        <p>Telefone: 0800 0 11 5566</p>
+        <p> E-mail: Natura@gmail.com</p>
+        <p>Instagram: @naturaOficial </p>
+        <p>© 2024 Natura.</p>
       </footer>
     );
   }
